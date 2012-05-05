a96d30b9b475d8cdd067b510c840aa04f8435c40
Justin B. Kay <justinbkay@gmail.com>
2012-05-05 11:34:48 -0600

    minor change to restart

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  app.use(express.session({secret: 'knoetchalewelake1'}));
+  app.use(express.session({secret: 'knoetchalewelake1sd'}));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
